242df9a7f526c1bd68fd50f95be20994da5edd02
Fred Snyder <fsnyder@castwide.com>
2019-04-08 13:31:10 -0400

    First commit of 2.0 after repo split.

---
+++ lib/gamefic-tty.rb
@@ -0,0 +1,10 @@
+require 'gamefic-tty/text'
+require 'gamefic-tty/version'
+
+module Gamefic
+  module Tty
+    autoload :Engine, 'gamefic-tty/engine'
+    autoload :User,   'gamefic-tty/user'
+    autoload :Shell,  'gamefic-tty/shell'
+  end
+end
---
+++ lib/gamefic-tty/text.rb
@@ -0,0 +1,8 @@
+module Gamefic
+  module Tty
+    module Text
+      autoload :Ansi, 'gamefic-tty/text/ansi'
+      autoload :Html, 'gamefic-tty/text/html'
+    end
+  end
+end
