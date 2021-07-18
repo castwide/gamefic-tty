180069668fbc2ce7872bc1d1f3ede7c84f92aeb5
Fred Snyder <fsnyder@castwide.com>
2021-07-18 00:53:50 -0400

    Test multiple commands in queue

--- spec/engine_spec.rb
+++ spec/engine_spec.rb
@@ -2,6 +2,11 @@ RSpec.describe Gamefic::Tty::Engine do
   let(:plot) {
     plot = Gamefic::Plot.new
     plot.stage do
+      respond :think do |actor|
+        actor.tell 'Player thinks'
+        actor.queue.push 'quit'
+      end
+
       respond :quit do |actor|
         actor.tell 'Player quit'
         actor.conclude default_conclusion
@@ -10,27 +15,33 @@ RSpec.describe Gamefic::Tty::Engine do
     plot
   }
 
-  let(:quitter) {
-    double(Gamefic::Tty::User, query: 'quit', update: nil)
-  }
-
   it 'completes a turn' do
-    engine = Gamefic::Tty::Engine.new(plot: plot, user: quitter)
+    user = Gamefic::Tty::User.new(input: StringIO.new('quit'), output: StringIO.new)
+    engine = Gamefic::Tty::Engine.new(plot: plot, user: user)
     engine.turn
     expect(engine.character).to be_concluded
   end
 
   it 'runs until concluded' do
-    engine = Gamefic::Tty::Engine.new(plot: plot, user: quitter)
+    user = Gamefic::Tty::User.new(input: StringIO.new('quit'), output: StringIO.new)
+    engine = Gamefic::Tty::Engine.new(plot: plot, user: user)
     engine.run
     expect(engine.character).to be_concluded
   end
 
-  it "runs from the singleton method" do
-    engine = Gamefic::Tty::Engine.run(plot: plot, user: quitter)
+  it 'runs from the singleton method' do
+    user = Gamefic::Tty::User.new(input: StringIO.new('quit'), output: StringIO.new)
+    engine = Gamefic::Tty::Engine.run(plot: plot, user: user)
     expect(engine.character).to be_concluded
   end
 
+  it 'handles multiple commands in queue' do
+    user = Gamefic::Tty::User.new(input: StringIO.new("think"), output: StringIO.new)
+    engine = Gamefic::Tty::Engine.new(plot: plot, user: user)
+    engine.run
+    expect(user.output.string).to include('Player thinks')
+    expect(user.output.string).to include('Player quit')
+  end
 
   it 'updates after conclusion' do
     user = Gamefic::Tty::User.new(input: StringIO.new('quit'), output: StringIO.new)
